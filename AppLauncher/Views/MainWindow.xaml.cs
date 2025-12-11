85088eb09ed831392d390301eebceb7a5686b6bd
bck123 <1836993098@qq.com>
2025-12-11 21:21:37 +0800

    测试git

--- AppLauncher/Views/MainWindow.xaml.cs
+++ AppLauncher/Views/MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace AppLauncher.Views
         private MainViewModel Vm => (MainViewModel)DataContext;
         public MainWindow()
         {
+
             InitializeComponent();
         }
 
