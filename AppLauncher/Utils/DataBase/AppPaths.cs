36b5aff9f03e82f4c6520665a8d626dbff0386d1
bck123 <1836993098@qq.com>
2026-01-22 09:13:06 +0800

    sqllite 冗余

---
+++ AppLauncher/Utils/DataBase/AppPaths.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLauncher.Utils.DataBase
+{
+    public static class AppPaths
+    {
+        public static string AppDir =>
+            Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "AppLauncher");
+
+        public static string DbPath =>
+            Path.Combine(AppDir, "data.db");
+    }
+}
---
+++ AppLauncher/Utils/DataBase/DbInitializer.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace AppLauncher.Utils.DataBase
+{
+  
+
+    public static class DbInitializer
+    {
+        public static void Initialize()
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(AppPaths.DbPath)!);
+
+            using var conn = new SqliteConnection(
+                $"Data Source={AppPaths.DbPath}");
+
+            conn.Open();
+
+            CreateTables(conn);
+            SeedData(conn);
+        }
+
+        private static void CreateTables(SqliteConnection conn)
+        {
+            conn.Execute("""
+        CREATE TABLE IF NOT EXISTS Categories (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Name TEXT NOT NULL UNIQUE
+        );
+
+        CREATE TABLE IF NOT EXISTS Shortcuts (
+            Id TEXT PRIMARY KEY,
+            DisplayName TEXT NOT NULL,
+            TargetPath TEXT NOT NULL,
+            Category TEXT NOT NULL
+        );
+        """);
+        }
+
+        private static void SeedData(SqliteConnection conn)
+        {
+            var count = conn.ExecuteScalar<int>(
+                "SELECT COUNT(*) FROM Categories");
+
+            if (count == 0)
+            {
+                conn.Execute(
+                    "INSERT INTO Categories (Name) VALUES (@name)",
+                    new { name = "全部" });
+            }
+        }
+    }
+
+}
